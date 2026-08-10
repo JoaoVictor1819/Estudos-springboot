cc783577a94ad12b1616389131f367d06848771a
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-08-09 21:08:59 -0300

    Ajuster nao definidos e sem resultados concretos

--- src/main/java/deveDojo/academy/aulasSpringboot_essentials/repository/AnimaisRepository.java
+++ src/main/java/deveDojo/academy/aulasSpringboot_essentials/repository/AnimaisRepository.java
@@ -3,8 +3,6 @@ package deveDojo.academy.aulasSpringboot_essentials.repository;
 import deveDojo.academy.aulasSpringboot_essentials.domain.Animais;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface AnimaisRepository extends JpaRepository <Animais, Long>{
 
 }
--- src/main/java/deveDojo/academy/aulasSpringboot_essentials/requests/AnimaisPostRequestBody.java
+++ src/main/java/deveDojo/academy/aulasSpringboot_essentials/requests/AnimaisPostRequestBody.java
@@ -1,7 +1,9 @@
 package deveDojo.academy.aulasSpringboot_essentials.requests;
 
 
+import jakarta.persistence.Column;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class AnimaisPostRequestBody {
