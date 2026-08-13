63381d8f8f9e47914ce01e9f902e4faf40fb7bcd
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-08-13 19:25:41 -0300

    Utilizando o WebMvcConfigurer para melhorar as paginas da tabela e criacao do configurer para ajuste do tamanho das paginas e numeros de elementos por pagina

--- src/main/java/deveDojo/academy/aulasSpringboot_essentials/repository/AnimaisRepository.java
+++ src/main/java/deveDojo/academy/aulasSpringboot_essentials/repository/AnimaisRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AnimaisRepository extends JpaRepository <Animais, Long>{
-
     List<Animais> findByName(String name);
 
 }
